05bf9f78da673a9230e2772ae65d07e4dc0d0971
215254589 <47609837+215254589@users.noreply.github.com>
2020-03-04 10:24:42 +0200

    Add files via upload

---
+++ Question2/src/main/java/za/ac/cput/Projects/Builder.java
@@ -0,0 +1,10 @@
+package za.ac.cput.Projects;
+
+public class Builder extends Man {
+
+    @Override
+    public String canWalk(String move) {
+
+        return "I can walk to work " + move;
+    }
+}
